60905ffcdbb06f4f6e8883411c171f29dc6f9cd7
jotasil88 <joaopauloluiz2005@gmail.com>
2025-08-21 15:19:48 -0300

    model.Department created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import model.Department;
+
 public class Program {
 
 	public static void main(String[] args) {
 
+		Department department = new Department(24, "gay");
 		
+		System.out.println(department);
 	}
 
 }
